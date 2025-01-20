cb640410e243fc44d14373272809fed853e3d0cb
SeHoon <sehoon1989@gmail.com>
2025-01-20 23:42:10 +0900

    feature/add-multiple-datasource

---
+++ src/main/java/com/sehoon/admintempleteserver/common/config/PrimaryDatabaseConfig.java
@@ -0,0 +1,51 @@
+package com.sehoon.admintempleteserver.common.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.sehoon.admintempleteserver.module.primary.mapper", sqlSessionFactoryRef = "primarySqlSessionFactory")
+public class PrimaryDatabaseConfig {
+
+    @Value("${spring.datasource.primary.url}")
+    private String url;
+
+    @Value("${spring.datasource.primary.username}")
+    private String username;
+
+    @Value("${spring.datasource.primary.password}")
+    private String password;
+
+    @Value("${spring.datasource.primary.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${mybatis.primary.mapper-locations}")
+    private String mapperLocations;
+
+    @Bean(name = "primaryDataSource")
+    public DataSource primaryDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean(name = "primarySqlSessionFactory")
+    public SqlSessionFactory primarySqlSessionFactory(@Qualifier("primaryDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
+        return sessionFactory.getObject();
+    }
+}
---
+++ src/main/java/com/sehoon/admintempleteserver/common/config/SecondaryDatabaseConfig.java
@@ -0,0 +1,51 @@
+package com.sehoon.admintempleteserver.common.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.sehoon.admintempleteserver.module.secondary.mapper", sqlSessionFactoryRef = "secondarySqlSessionFactory")
+public class SecondaryDatabaseConfig {
+
+    @Value("${spring.datasource.secondary.url}")
+    private String url;
+
+    @Value("${spring.datasource.secondary.username}")
+    private String username;
+
+    @Value("${spring.datasource.secondary.password}")
+    private String password;
+
+    @Value("${spring.datasource.secondary.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${mybatis.secondary.mapper-locations}")
+    private String mapperLocations;
+
+    @Bean(name = "secondaryDataSource")
+    public DataSource secondaryDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean(name = "secondarySqlSessionFactory")
+    public SqlSessionFactory secondarySqlSessionFactory(@Qualifier("secondaryDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
+        return sessionFactory.getObject();
+    }
+}
